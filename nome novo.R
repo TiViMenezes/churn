e56057824f992b43a2da431e610b72f97fc0a8cf
TiViMenezes <tiago.menezes@ibpad.com.br>
2017-09-23 11:35:13 -0300

    tiago

---
+++ nome novo.R
@@ -0,0 +1,5 @@
+#para baixar bases publicas - ir no github pesquisar por projetos ou perfis, 
+#baixar em ssh e criar novo projeto em git no R syudio
+#manter arquivos em csv - para facilitar a leitura
+
+#importar base
